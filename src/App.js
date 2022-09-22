21be5634d1b5d610e7175f220aef22f0ef008f04
ignacio gaston odella <108488609+Nano92OB@users.noreply.github.com>
2022-09-22 20:32:09 -0300

    first step

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { CardGenerator } from './Services/CardGenerator';
+import Panel from './components/Panel';
 
 function App() {
 
@@ -8,20 +9,7 @@ function App() {
   console.log(card)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Panel/>
     </div>
   );
 }
