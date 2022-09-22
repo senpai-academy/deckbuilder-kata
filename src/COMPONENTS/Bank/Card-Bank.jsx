7d6f3283ff771257734c88776abecf6c9b0465fc
BadSh4dow <guidocabrera711@gmail.com>
2022-09-22 20:18:57 -0300

    Empezamos

---
+++ src/COMPONENTS/Bank/Card-Bank.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Card } from "../../Services/Card";
+
+const CardBank =()=>{
+    return(
+        <React.Fragment>
+            <Card/>
+        </React.Fragment>
+    )
+}
+export default CardBank;
---
+++ src/COMPONENTS/Bank/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = (props) =>{
+    return(
+        <React.Fragment>
+            
+
+        </React.Fragment>
+    )
+}
+export default Card;
