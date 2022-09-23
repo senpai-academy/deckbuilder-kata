526e79b03b975178dfb57f61aa7a6462704fd56d
gero <geronicola1696@gmail.com>
2022-09-22 22:20:50 -0300

    comienzo card-bank

--- src/App.js
+++ src/App.js
@@ -6,10 +6,22 @@ function App() {
 
   const card = new CardGenerator().generateCard();
   console.log(card)
+
   return (
     <div id="GridMain">
         <div id="DeckGrid" className="backGround">a</div>
-        <div id="CardBank" className="backGround">b</div>
+        <div id="CardBank" className="backGround">
+            <ul>
+              <li className='flex-between fila-lista'>
+                  <div className='img-mana'>
+                      7 
+                  </div>
+                  <span className='text-bank'> Fiery Bolt </span>
+                  <div></div>
+              </li>
+
+            </ul>
+        </div>
         <div id="DeckInfo" className="backGround">c</div>
     </div>
   );
