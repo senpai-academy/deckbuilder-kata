6db45b00f5bfa03194bc2a542c9fc42918f19a32
ignacio gaston odella <108488609+Nano92OB@users.noreply.github.com>
2022-09-27 19:25:50 -0300

    continue

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,27 @@
 import React from "react";
-import { Loading } from "../components/Lib"
+import { Loading } from "../components/Lib";
 
-const CardList = ({myCards}) =>{
-    return (
-        (myCards) ?
-        <div className="panel panel-right">
-            {myCards.map((card, index) => (
-                <Card idx={index} data={card}></Card>            
-            ))}
-        </div>: <Loading/>
-    )
-}
-
-
-
+const CardList = ({ myCards }) => {
+  return myCards ? (
+    <div className="panel panel-right">
+      <ul className="card-list">
+        {myCards.map((card, index) => (
+          <Card idx={index} data={card}></Card>
+        ))}
+      </ul>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
 
 const Card = (props) => {
-    return(
+  return (
+    <li>
+      <span>{props.data.cost}</span>
+      {props.data.name}
+    </li>
+    /*
         <div className="panel panel-right"> 
         <div>
             <div className="card-cost">
@@ -30,7 +35,8 @@ const Card = (props) => {
         </div>
 
         </div>
-    )
-}
+        */
+  );
+};
 
-export  {CardList,Card};
+export { CardList, Card };
