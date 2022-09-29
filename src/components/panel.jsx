b5529414aefcc0a3f48c0ce666b4dcb53ea3b73a
Yayi728 <yaelcalcagno@gmail.com>
2022-09-29 20:29:41 -0300

    casi panel

--- src/components/panel.jsx
+++ src/components/panel.jsx
@@ -3,27 +3,28 @@ import React from "react";
 const Panel = (props) =>{
     return (
         
-        <div className={"card-container-modal"}>
+        <div className={"container"}>
             {
                 props.cards.map((card,key)=>{
                     return (
-                        <div key={key}>
-                            <div className="card-title">
-                                <div className='relative'>
-                                    <span className='numero'>{card.cost}</span>
-                                    <div className='img-mana'></div>
-                                </div>   
-                                <span className="text-title">{card.name}</span>
-                            </div>
-                            <div className="card-img">
-                                <img src={`/Sprites/CardArt/${card.art}`} alt={card.name}/>
-                            </div>
-                                <div className="card-details">
-                                    <img alt="a" className={"icon-details "+(card.rarity.value)} height={'30px'} src={`/Sprites/Icons/${card.cardType}.png`} />
-                                    <span><img alt="b" className="icon-details" height={'30px'} src={`/Sprites/Icons/Attack.png`} />{card.attackScore}</span>
-                                    <span><img alt="c" className="icon-details" height={'30px'} src={`/Sprites/Icons/Defense.png`} />{card.defenseScore}</span>
+
+                            <div className={"card-container"} key={key}>
+                                <div className="card-title-deck">
+                                    <div className='relative'>
+                                        <span className='numero'>{card.cost}</span>
+                                        <div className='img-mana'></div>
+                                    </div>   
+                                    <span className="text-title">{card.name}</span>
                                 </div>
-                        </div>
+                                <div className="card-img-panel">
+                                    <img src={`/Sprites/CardArt/${card.art}`} alt={card.name}/>
+                                </div>
+                                    <div className="card-details-panel">
+                                        <img alt="a" className={"icon-details "+(card.rarity.value)} height={'30px'} src={`/Sprites/Icons/${card.cardType}.png`} />
+                                        <span><img alt="b" className="icon-details" height={'30px'} src={`/Sprites/Icons/Attack.png`} />{card.attackScore}</span>
+                                        <span><img alt="c" className="icon-details" height={'30px'} src={`/Sprites/Icons/Defense.png`} />{card.defenseScore}</span>
+                                    </div>
+                            </div>  
                     )
                 })
             }
