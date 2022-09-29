6df6a4c4122f7f05838ecd0adfe714a10ac812f5
joaccocaballero <joaquincaballero182@gmail.com>
2022-09-29 19:45:54 -0300

    stats

--- src/Services/CardSettings.js
+++ src/Services/CardSettings.js
@@ -2,9 +2,9 @@ import { Rarity, RarityValue } from "./Card";
 
 export class CardSettings{
     constructor(){
-        this.lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        this.minDescriptionLength = 25;
-        this.maxDescriptionLength = 45;
+        this.lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+        this.minDescriptionLength = 5;
+        this.maxDescriptionLength = 20;
         this.minAttack = 1;
         this.maxAttack = 6;
         this.minCost = 0;
